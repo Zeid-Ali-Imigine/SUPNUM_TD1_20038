35a7ca92ae4e68a2674d71d05d5b61bb87f3bae2
Zeid-Ali-Imigine <23038@gmail.com>
2025-11-14 20:49:49 +0000

    initial project parametters

---
+++ surveying-supervising/src/main/java/com/servers/surveying_supervising/SurveyingSupervisingApplication.java
@@ -0,0 +1,13 @@
+package com.servers.surveying_supervising;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyingSupervisingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyingSupervisingApplication.class, args);
+	}
+
+}
---
+++ surveying-supervising/src/test/java/com/servers/surveying_supervising/SurveyingSupervisingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servers.surveying_supervising;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyingSupervisingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
